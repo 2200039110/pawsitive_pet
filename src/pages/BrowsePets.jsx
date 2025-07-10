b13db18ba2075b78bad1cd5034ca9a0c9975d723
Chitturi Ashitha <168183390+2200039110@users.noreply.github.com>
2025-07-10 15:53:26 +0530

    Update BrowsePets.jsx

--- src/pages/BrowsePets.jsx
+++ src/pages/BrowsePets.jsx
@@ -13,7 +13,7 @@ const BrowsePets = () => {
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
-    fetch('https://pawsitive-pet.onrender.com/pets')
+    fetch('https://pawsitive-pet.onrender.com/api/pets')
       .then(res => res.json())
       .then(data => setPets(data));
   }, []);
